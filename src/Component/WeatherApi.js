4afa2c2431404160c148c625dcd4b173bc86ae19
Praveen Nathan Chokkar <45989474+praveennathanc@users.noreply.github.com>
2021-06-14 09:36:29 +0530

    init

--- src/Component/WeatherApi.js
+++ src/Component/WeatherApi.js
@@ -1,36 +1,53 @@
 import axios from 'axios'
 import React,{useState,useEffect} from 'react'
 
+
 function WeatherApi() {
 
     const[post,setPost] = useState({});
-    const[id,setId] = useState();
+    const [post1,setPost1] = useState({})
+    const[climate,setClimate] =useState({})
+    const[id,setId] = useState("chennai");
 
     useEffect(()=>
     {
-        axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`)
+        axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${id}&appid=23cc9753908f660aea8acbf796c64133`)
         .then(response => {
             console.log(response)
             setPost(response.data)
+            setPost1(response.data.main)
+            setClimate(response.data.weather[0])
         })
         .catch(error => {   
             console.log(error)
         })
+
     },[id])
 
     return (
         
-        <div className="api-container">
-            <h1>Dummy API Call</h1>
-            <br/>
-            <input type="text" placeholder="Enter Any Number" value={id} onChange={e=>setId(e.target.value)}/>
-           <div className="api-c">
-               {
-                 id ?  <h2>{post.title}</h2> : <h2>Dummy Text Will Appear Here</h2>
-               }
-              
-           </div>
-        </div>
+        <div className="weatherContainer">
+            <h3>Weather API Call</h3>
+            <br></br>
+                <div className="inputField">
+                    <input type="text" value={id} onChange={e=>setId(e.target.value)} placeholder="Enter The Location"/>
+                </div>
+            <div className="weatherDetails">
+            <h1>{post.name}</h1>
+
+            <div className="temp">
+            <h2>{Math.round(post1.temp_min -273.15)}<sup>&deg;C</sup></h2><small>Min</small>
+            <h2>{Math.round(post1.temp_max -273.15)}<sup>&deg;C</sup></h2><small>Max</small>
+            </div>
+                  <div>
+                    <img src={`http://openweathermap.org/img/wn/${climate.icon}@2x.png`} alt="weatherIcon"/>
+                <h4>{climate.main}</h4>
+                  </div>
+            </div>
+            </div>
+         
+           
+    
     )
 }
 
